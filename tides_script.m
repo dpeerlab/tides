4ca85d950cd03f9c0a984b5f770d40ea90147198
sharma.roshan <sharma.roshan430@gmail.com>
2017-12-15 13:24:23 -0500

    Add files via upload

--- tides_script.m
+++ tides_script.m
@@ -7,12 +7,15 @@ loc = pwd;
 addpath(genpath(loc));
 
 % load the fcs file
-file = cytof_data_min('filename.fcs');
+file = cytof_data_min('EM141482.fcs');
+
+% arch-sinh transform the data with a co-factor of 5:
+file = file.transform_data(5);
 
 % three markers of interest:
-marker1 = 'marker1';
-marker2 = 'marker2';
-marker3 = 'marker3';
+marker1 = 'il-7ra';
+marker2 = 'perforin';
+marker3 = 't-bet';
 
 % get the N x 3 matrix of the three markers
 data = file.get_data({marker1, marker2, marker3});
@@ -31,21 +34,66 @@ maxz = max(data(:, 3));
 
 % If slices of DREVI needed, it is a vector of locations where DREVI is
 % needed. The following will produce a DREVI slice at x = 0.1 only.
-get_drevi_slices_at = [0.1];
+get_drevi_slices_at = linspace(minx, maxx, 40);
 
 % get threeD-DREVI:
 % Can turn off some of the inputs:
 % display_plots will display the plots
 % smooth will smooth the surfaces
 % slice plots the DREVI slice   
-
+figure;
 file.threeD_edge_visualize(marker1, marker2, marker3, get_drevi_slices_at, 'display_plots', 'smooth', 'slice', ...
         'minx', minx, 'miny', miny, 'minz', minz, 'maxx', maxx, 'maxy', maxy, 'maxz', maxz);
+xlabel(marker1)
+ylabel(marker2)
+zlabel(marker3)
+    
+% If you want to make a video of the slices, please use the following code:
+clearvars M;
+name_of_video = 'example.avi';
+v = VideoWriter(name_of_video);
+v.FrameRate = 3;
+N=length(get_drevi_slices_at); % Number of frames
+j = linspecer(256);
+for i = 1:N
+    %figure();
+    file.threeD_edge_visualize(marker1, marker2, marker3, get_drevi_slices_at(i), j, 'smooth', 'slice');       
+    xlabel(marker1)
+    ylabel(marker2)
+    zlabel(marker3)
+    M(i)=getframe(gcf); % leaving gcf out crops the frame in the movie.
+end
+%movie2avi(M, name_of_video, 'fps', 4.5);
+open(v)
+writeVideo(v, M);
+close(v);
+    
     
 % get TIDES. The TIDES curve may need to be smoothed.
 [tides_scores, trajectory_points] = file.compute_windowed_DREMI_interpolate(marker1, marker2, marker3, number_of_tides_steps, noise_threshold, ...
         'minx', minx, 'miny', miny, 'minz', minz, 'maxx', maxx, 'maxy', maxy, 'maxz', maxz);
+figure; 
+plot(trajectory_points, tides_scores, 'LineWidth', 2)
+axis tight
+box off
+xlabel(marker1)
+ylabel('DREMI')
+title([marker2, ' -> ', marker3])
+set(gca, 'FontSize', 14)
 
+% if you want to smooth the TIDES curve:
+[X, Y] = make_trend_plots(trajectory_points(:), tides_scores(:), 'smoothing_factor', 0.5);
+figure; 
+plot(X, Y, 'LineWidth', 2)
+axis tight
+box off
+xlabel(marker1)
+title([marker2, ' -> ', marker3])
+ylabel('DREMI')
+set(gca, 'FontSize', 14)
+    
 % get threeD-dremi
 dremi_score = file.compute_threeD_dremi(marker1, marker2, marker3, noise_threshold, ...
-        'minx', minx, 'miny', miny, 'minz', minz, 'maxx', maxx, 'maxy', maxy, 'maxz', maxz);               
+        'minx', minx, 'miny', miny, 'minz', minz, 'maxx', maxx, 'maxy', maxy, 'maxz', maxz);
+    
+disp(['The 3D-DREMI score is: ' num2str(dremi_score, 3)])
